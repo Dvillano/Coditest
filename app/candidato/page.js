42db9425cbe46efc00dbbbc1af982cc99479e01d
Dvillano <danie.villano@gmail.com>
2023-08-18 20:49:04 -0300

    Fix ruta candidato

---
+++ app/candidato/page.js
@@ -0,0 +1,9 @@
+import Registration from "./components/register/Registration";
+
+export default function Home() {
+    return (
+        <>
+            <h1>Formulario candidato</h1>
+        </>
+    );
+}
