0e227ba38c78fc41a6893df247830949bf92cc8b
Dvillano <danie.villano@gmail.com>
2023-08-18 20:51:50 -0300

    Deleted candidato

--- app/candidato/page.js
+++
@@ -1,9 +0,0 @@
-import Registration from "./components/register/Registration";
-
-export default function Home() {
-    return (
-        <>
-            <h1>Formulario candidato</h1>
-        </>
-    );
-}
