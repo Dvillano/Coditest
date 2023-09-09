46ee03aa6281210615539b7399b9951ad58da3db
Dvillano <danie.villano@gmail.com>
2023-09-09 19:12:38 -0300

    Added detailModal for problemManagement

--- app/components/Admin/ProblemManagement/ProblemManagement.jsx
+++ app/components/Admin/ProblemManagement/ProblemManagement.jsx
@@ -61,6 +61,7 @@ function ProblemManagement() {
                             className="flex items-center gap-3"
                             variant="gradient"
                             size="sm"
+                            color="green"
                         >
                             <PlusIcon strokeWidth={2} className="h-4 w-4" />{" "}
                             Agregar problema
