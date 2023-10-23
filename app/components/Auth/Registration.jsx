397883c9bc8963480818b57cf210bb21c5f57428
Dvillano <danie.villano@gmail.com>
2023-10-23 20:35:32 -0300

    Fix autocomplete registration mail

--- app/components/Auth/Registration.jsx
+++ app/components/Auth/Registration.jsx
@@ -171,6 +171,7 @@ const Registration = () => {
                         </label>
                         <input
                             id="password"
+                            autocomplete="off"
                             className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
                             type="password"
                             value={password}
