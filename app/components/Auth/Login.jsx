02f851c90fd42f5180910a19d95f57969888972c
Dvillano <danie.villano@gmail.com>
2023-10-23 22:24:51 -0300

    Fix register route error on production

--- app/components/Auth/Login.jsx
+++ app/components/Auth/Login.jsx
@@ -97,7 +97,6 @@ const Login = () => {
                             Email
                         </label>
                         <input
-                            autocomplete="off"
                             id="email"
                             className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
                             type="email"
